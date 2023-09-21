b3207aa635d54c582bde7ee16b437307da4acd7d
matsu <50922910+mtsml@users.noreply.github.com>
2023-09-22 02:12:11 +0900

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '@/styles/globals.css';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const gtag = process.env.NEXT_PUBLIC_GA_ID;
+  return (
+    <>
+      {gtag && 
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`} />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag}');
+            `}
+          </Script>
+        </>
+      }
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
