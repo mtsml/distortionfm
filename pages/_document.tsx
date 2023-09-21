c37be7540e0fffef2074e22d1005caadd2cd0565
matsu <50922910+mtsml@users.noreply.github.com>
2023-09-22 01:47:01 +0900

    add Next.js

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Distortion.fm</title>
+        <link rel="icon" href="favicon.png" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"
+            integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
---
+++ pages/episode/[id].tsx
@@ -0,0 +1,99 @@
+import { useEffect } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import Parser from "rss-parser";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { getIdFromAnchorRssFeedItem } from "@/util/utility";
+
+interface Episode {
+  id: string;
+  title: string;
+  isoDate: string;
+  description: string;
+}
+
+interface Props {
+  episode: Episode;
+} 
+
+const Episode = ({ episode }: Props) => {
+
+  useEffect(() => {
+    const main = document.getElementsByTagName("main")[0];
+
+    // description
+    const parser = new DOMParser();
+    const parsedDescription = parser.parseFromString(episode.description, "text/html")
+    const descriptions = Array.from(parsedDescription.body.childNodes);
+    descriptions.forEach(description => main.appendChild(description));
+
+    // isoDate
+    const isoDate = document.createElement("p");
+    isoDate.innerText = episode.isoDate;
+    main.appendChild(isoDate);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{episode.title}</title>
+      </Head>
+      <div className="pure-g">
+        <Header />
+        <div className="contents pure-u-1 pure-u-xl-3-4">
+          <main>
+            <h1>{episode.title}</h1>
+            <iframe
+              src={`https://podcasters.spotify.com/pod/show/matsumaru/embed/episodes/${episode.id}`}
+              width="100%"
+              frameBorder="0"
+              scrolling="no"
+            ></iframe>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const parser = new Parser();
+  const feed = await parser.parseURL('https://anchor.fm/s/db286500/podcast/rss');
+
+  const paths = feed.items.map(item => {
+    const id = getIdFromAnchorRssFeedItem(item);
+    return {
+      params: { id }
+    }
+  });
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params ? context.params.id as string : "";
+
+  const parser = new Parser();
+  const feed = await parser.parseURL('https://anchor.fm/s/db286500/podcast/rss');
+
+  const episode = feed.items.find(item => item.link?.includes(id)) || {};
+  const { title, isoDate, content } = episode;
+
+  return {
+    props: {
+      episode: {
+        id,
+        title,
+        isoDate,
+        description: content
+      }
+    }
+  }
+}
+
+export default Episode;
