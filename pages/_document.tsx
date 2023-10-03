fbcc150e05cf2d624c6ff69cd246904558f232ce
matsu <50922910+mtsml@users.noreply.github.com>
2023-10-04 00:08:00 +0900

    faviconを変更

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ const Document = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="1995年生まれの男性が友達と雑談をする" />
         <title>Distortion.fm</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"
             integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css" />
