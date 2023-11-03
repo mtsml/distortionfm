c68b05290796502419e9efd456d354568c810f93
matsu <50922910+mtsml@users.noreply.github.com>
2023-11-04 02:37:45 +0900

    PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+});
