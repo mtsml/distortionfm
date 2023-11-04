5acaf972e103d37ff42a4d95ffa7faebd4ea51ac
matsu <50922910+mtsml@users.noreply.github.com>
2023-11-04 16:07:02 +0900

    Fontawesomeを導入

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <header className="pure-u-1 pure-u-xl-1-4">
       <Link href="/">Distortion.fm</Link>
-      <p>1995年生まれの男性が友達と雑談をする。</p>
+      <p>
+        1995年生まれの男性が友達と雑談をする。
+        <FontAwesomeIcon
+          className="notification"
+          icon={faBell}
+          size="xl"
+          onClick={() => alert("navigation")}
+        />
+      </p>
     </header>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const gtag = process.env.NEXT_PUBLIC_GA_ID;
