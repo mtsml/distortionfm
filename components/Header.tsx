d6c4f39c4add959f5d71a2438a8c9585ac55ff9d
matsu <50922910+mtsml@users.noreply.github.com>
2023-11-04 16:51:52 +0900

    Notification API

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const registerNotification = () => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        const title = "通知確認";
+        const options = {
+          body: "通知設定が完了しました。こんな感じで最新エピソードの追加をお知らせするよ。",
+          icon: "/favicon.ico"
+        }
+        new Notification(title, options);
+      }
+    });
+  };
+
   return (
     <header className="pure-u-1 pure-u-xl-1-4">
       <Link href="/">Distortion.fm</Link>
@@ -12,7 +25,7 @@ const Header = () => {
           className="notification"
           icon={faBell}
           size="xl"
-          onClick={() => alert("navigation")}
+          onClick={registerNotification}
         />
       </p>
     </header>
