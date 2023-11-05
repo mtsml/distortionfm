5dcc073276084ad9a321ced570e7942d2d9c35c2
matsu <50922910+mtsml@users.noreply.github.com>
2023-11-06 02:39:48 +0900

    通知をクリックすると指定のURLを開く

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,8 +21,18 @@ self.addEventListener("push", (event) => {
       data.title,
       {
         body: data.body,
-        icon: "/favicon.ico"
+        icon: "/favicon.ico",
+        data: {
+          url: data.url
+        }
       }
     )
   );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data?.url || "/")
+  )
 });
