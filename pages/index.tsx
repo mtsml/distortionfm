8878d461a0588cb795ed0c7db7cbe21384eb91ce
matsu <50922910+mtsml@users.noreply.github.com>
2023-11-15 02:03:20 +0900

    エピソードリストに話者情報を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import { GetStaticProps } from "next";
 import Link from "next/link";
 import Parser from "rss-parser";
+import { sql } from "@vercel/postgres";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { getIdFromAnchorRssFeedItem } from "@/util/utility";
+
+interface Speaker {
+  id: number;
+  name: string;
+  icon: IconProp;
+}
 
 interface Episode {
   id: string;
   title: string;
   isoDate: string;
+  speakers: Speaker[];
 }
 
 interface Props {
@@ -28,6 +38,14 @@ const Home = ({ episodes }: Props) => {
                 <Link href={`/episode/${encodeURIComponent(episode.id)}`} className="pure-menu-link">
                   {episode.title}
                 </Link>
+                {episode.speakers.map(speaker => (
+                  <FontAwesomeIcon
+                    key={speaker.id}
+                    className="speaker-icon"
+                    icon={speaker.icon}
+                    size="sm"
+                  />
+                ))}
               </div>
             ))}
           </div>
@@ -42,15 +60,24 @@ export const getStaticProps: GetStaticProps = async () => {
   const parser = new Parser();
   const feed = await parser.parseURL('https://anchor.fm/s/db286500/podcast/rss');
 
+  const { rows } = await sql`
+    SELECT episode_id, speaker_id as id, name, icon
+    FROM episode_speaker_map esm
+    INNER JOIN speaker ON speaker.id = esm.speaker_id
+    ORDER BY episode_id, speaker_id
+  `;
+
   const episodes = feed.items.map(item => {
     const id = getIdFromAnchorRssFeedItem(item);
     const title = item.title;
     const isoDate = item.isoDate;
+    const speakers = rows.filter(row => row.episode_id === id);
 
     return {
       id,
       title,
-      isoDate
+      isoDate,
+      speakers
     }
   });
 
